e0dd2beb787f171c4a907d6bbccbe027ab91803b
aashmelev <a.a.shmelev@gmail.com>
2018-05-18 21:29:34 +0300

    Added tests

--- tests/Doctrine/Tests/DBAL/Driver/PDOSqlsrv/DriverTest.php
+++ tests/Doctrine/Tests/DBAL/Driver/PDOSqlsrv/DriverTest.php
@@ -4,6 +4,7 @@ namespace Doctrine\Tests\DBAL\Driver\PDOSqlsrv;
 
 use Doctrine\DBAL\Driver\PDOSqlsrv\Driver;
 use Doctrine\Tests\DBAL\Driver\AbstractSQLServerDriverTest;
+use PDO;
 
 class DriverTest extends AbstractSQLServerDriverTest
 {
@@ -15,5 +16,58 @@ class DriverTest extends AbstractSQLServerDriverTest
     protected function createDriver()
     {
         return new Driver();
+    }
+
+    public function testConnectionOptions()
+    {
+        $this->skipWhenNotUsingPdoSqlsrv();
+
+        $connection = $this->getConnection([
+            'APP' => 'APP_NAME',
+            'WSID' => 'WSID_NAME',
+        ]);
+
+        $result = $connection->query('select APP_NAME() as app,  HOST_NAME() as host')->fetch();
+
+        self::assertSame('APP_NAME', $result['app']);
+        self::assertSame('WSID_NAME', $result['host']);
+    }
+
+    public function testDriverOptions()
+    {
+        $this->skipWhenNotUsingPdoSqlsrv();
+
+        $connection = $this->getConnection([
+            PDO::ATTR_CASE => PDO::CASE_UPPER,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
+
+        self::assertSame(PDO::CASE_UPPER, $connection->getAttribute(PDO::ATTR_CASE));
+        self::assertSame(PDO::FETCH_ASSOC, $connection->getAttribute(PDO::ATTR_DEFAULT_FETCH_MODE));
+    }
+
+    private function getConnection($driverOptions)
+    {
+        return $this->createDriver()->connect(
+            [
+                'host' => $GLOBALS['db_host'],
+                'port' => $GLOBALS['db_port'],
+            ],
+            $GLOBALS['db_username'],
+            $GLOBALS['db_password'],
+            $driverOptions
+        );
+    }
+
+    /**
+     * @throws \PHPUnit_Framework_SkippedTestError
+     */
+    private function skipWhenNotUsingPdoSqlsrv()
+    {
+        if (isset($GLOBALS['db_type']) && $GLOBALS['db_type'] === 'pdo_sqlsrv') {
+            return;
+        }
+
+        $this->markTestSkipped('Test enabled only when using pdo_sqlsrv specific phpunit.xml');
     }
 }
